7e24c24611ad912340e3a67b4bf9f7342c72ca3e
Andrew Lechowicz <andrew@alecho.com>
2015-10-25 20:07:04 -0400

    Install EmberFire

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'time-tracker',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://simply-time.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
